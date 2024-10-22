6406874306275d2e7007d3fe5dad292f72b44664
MartijnCBV <49876349+MartijnCBV@users.noreply.github.com>
2024-10-22 22:36:53 +0200

    Some more plots

--- analysis/src/graphs.py
+++ analysis/src/graphs.py
@@ -4,16 +4,26 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 
-def lin_graph(data, xl: str, yl: str, path: str):
+def lin_graph(data, xl: str, yl: str, path: str, xs: list[int]=[], dashcycle=False):
     plt.rcParams.update({'font.size': 14})
     ks = list(data[0].keys())
-    xs = list(range(1, len(data)+1))
+    if xs == []:
+        xs = list(range(1, len(data)+1))
     ys = {k: [e[k] for e in data] for k in ks}
+    cyc = True
     for k in ks:
-        plt.plot(xs, ys[k], label=k)
+        if dashcycle:
+            if cyc:
+                plt.plot(xs, ys[k], label=k, linestyle=(0, (3, 3)))
+            else:
+                plt.plot(xs, ys[k], label=k, linestyle=(3, (3, 3)))
+            cyc = not cyc
+        else:
+            plt.plot(xs, ys[k], label=k)
     plt.xlabel(xl)
     plt.ylabel(yl)
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+    plt.legend(bbox_to_anchor=(0.5, -0.2), loc='upper center', ncol=3, borderaxespad=0., frameon=False)
+    plt.tight_layout()
     plt.savefig(path, format="pdf", bbox_inches='tight')
     plt.clf()
 
@@ -62,3 +72,41 @@ def memory_acc(data: Dict[str, Bench], path: str):
         proc.put(int(i)-1, dat)
     bar_graph(proc, "Index", "Memory(MB)", path)
 
+def timing(data: Dict[str, Bench], xl: str, path: str, xs: list[int]=[], dashcycle=False):
+    proc = np.empty(len(data.keys()), object)
+    for key in data:
+        i = re.sub(r"\D", "", key)
+        dat = {
+            "time(avg)": data[key]["pytimer"]["avgtim"],
+            "time(best)": data[key]["pytimer"]["besttim"]
+        }
+        proc.put(int(i)-1, dat)
+    lin_graph(proc, xl, "Time(sec)", path, xs=xs, dashcycle=dashcycle)
+
+def memory(data: Dict[str, Bench], xl: str, path: str, xs: list[int]=[], dashcycle=False):
+    proc = np.empty(len(data.keys()), object)
+    for key in data:
+        i = re.sub(r"\D", "", key)
+        dat = {
+            "glpsol(avg)": data[key]["solverr"]["avgmem"],
+            "glpsol(best)": data[key]["solverr"]["bestmem"]
+        }
+        proc.put(int(i)-1, dat)
+    lin_graph(proc, xl, "Memory(MB)", path, xs=xs, dashcycle=dashcycle)
+
+def timing_double(a: Dict[str, Bench], b: Dict[str, Bench], al, bl, xl: str, path: str, xs: list[int]=[], dashcycle=False):
+    proc = np.empty(len(a.keys()), object)
+    for key in a:
+        i = re.sub(r"\D", "", key)
+        dat = {
+            f"{al}(avg)": a[key]["pytimer"]["avgtim"],
+            f"{al}(best)": a[key]["pytimer"]["besttim"]
+        }
+        proc.put(int(i)-1, dat)
+    for key in b:
+        i = re.sub(r"\D", "", key)
+        dat = proc.item(int(i)-1)
+        dat[f"{bl}(avg)"] = b[key]["pytimer"]["avgtim"]
+        dat[f"{bl}(best)"] = b[key]["pytimer"]["besttim"]
+        proc.put(int(i)-1, dat)
+    lin_graph(proc, xl, "Memory(MB)", path, xs=xs, dashcycle=dashcycle)
--- analysis/src/main.py
+++ analysis/src/main.py
@@ -8,3 +8,13 @@ if __name__ == "__main__":
     data = cut_bench(data, 10)
     timing_acc(data, os.path.abspath("../data/plots/timing_accuracy.pdf"))
     memory_acc(data, os.path.abspath("../data/plots/memory_accuracy.pdf"))
+    # overlap
+    data_a = read_bench(os.path.abspath("../data/general_no_overlap.json"))
+    data_a = cut_bench(data_a, 40)
+    data_b = read_bench(os.path.abspath("../data/general_overlap.json"))
+    data_b = cut_bench(data_b, 40)
+    timing_double(data_a, data_b, "No Overlap", "Overlap", "Instance", os.path.abspath("../data/plots/overlap_time.pdf"))
+    # no. borrels
+    data = read_bench(os.path.abspath("../data/increasing_borrels.json"))
+    timing(data, "No. Borrels", os.path.abspath("../data/plots/no_borrels_time.pdf"), dashcycle=True)
+    memory(data, "No. Borrels", os.path.abspath("../data/plots/no_borrels_mem.pdf"), dashcycle=True)
